49d8a1c3aea77b4e063bd003fe273a8684b9dc3c
Deblock Thomas <deblock.thomas.62@gmail.com>
2020-06-17 20:55:05 +0200

    refactor: use svgwrite insteadof drawSvg
    #4

--- custom_components/proscenic/vacuum_map_generator.py
+++ custom_components/proscenic/vacuum_map_generator.py
@@ -1,6 +1,6 @@
 import base64
 import struct
-import drawSvg as draw
+import svgwrite
 
 def build_map(m, track, file_path):
     inp = base64.b64decode(m)
@@ -66,17 +66,24 @@ def build_map(m, track, file_path):
     inp = base64.b64decode(track)
     path = struct.unpack('<' + 'b'*(len(inp)-4), inp[4:])
 
-    d = draw.Drawing(500, 550)
+    dwg = svgwrite.Drawing(file_path, size=(500,500))
+    
     for i in range(len(wallx)):
-        d.append(draw.Rectangle(wallx[i] * 5, 500 - (wally[i] * 5), 5, 5, fill='#000000', fill_opacity=0.7))
+        dwg.add(dwg.rect(insert=(wallx[i] * 5, 500 - (wally[i] * 5)), size=(5, 5), fill='#000000', fill_opacity=0.7))
 
     for i in range(len(floorx)):
-        d.append(draw.Rectangle(floorx[i] * 5, 500 - (floory[i] * 5), 5, 5, fill='#000000', fill_opacity=0.3))
+        dwg.add(dwg.rect(insert=(floorx[i] * 5, 500 - (floory[i] * 5)), size=(5, 5), fill='#000000', fill_opacity=0.3))
 
     draw_path = [((coord * 5) + 2.5 if i % 2 == 0 else (500 - (coord * 5)) + 2.5) for i, coord in enumerate(path)]
-    d.append(draw.Lines(*draw_path, fill="white", fill_opacity=0, stroke = 'black', stroke_opacity = 0.8))
+    dwg.add(dwg.circle(center = (draw_path[-2], draw_path[-1]), r = 2.5, fill='#000000', fill_opacity=0.65))
 
-    d.saveSvg(file_path)
+    dwg_path = dwg.path(['M{},{}'.format(draw_path[0], draw_path[1])], fill="white", fill_opacity=0, stroke = 'black', stroke_opacity = 0.6)
+
+    for i in range(len(draw_path) // 2):
+        dwg_path.push('L{},{}'.format(draw_path[2*i], draw_path[2*i+1]))
+    
+    dwg.add(dwg_path)
+    dwg.save()
 
 #m = "AAAAAAAAZABkwvIAFUDXABXCVUDVABaqwlXVAFbCqqlA1ABmw6pUVUDSAGbDqqTCVVDRAFbDqqVqqpDRAFbDqqVVqJDRAFqqqaqlVqSQ0QBaqpZqwqqkkNEAWqqZmsKqpJDRAFqqmWrCqqSQ0QAVVapawqqkkNIABVVawqqkkNQAFsKqpJDUABbCqqWQ1AAWwqqplNQAFsOqpdQAGsOqqUDTABrDqppQ0wAaw6qmkNMAFWrCqplQ0wAFw6qZQNMABcOqmkDTAAXDqplA0wAFw6pJQNMABcOqSdQABalqqkpA0wBWqVqqolDTAGqkFqqmkNIAAWqkBqqklNIAAaqQBqqkkNIAAaqQBqqkkNIAAZqQGqqklVTRAAGqkCqqpaqk0QABqpAqw6qoFNAAAaqQFqqlqpqk0AAGqpAqqqWqkKTQAAaqkCrDqlWU0AAGqpQaqsKWqZDQAAaqpGqqlqqpoNAABaqpasKqpalo0AABWqqawqpWqmqA0AAaxKpVwqrRABVVaqpaw6rSAAFVmcSq0wABVVbCVVbVAAVAAALYAALYAAVA0P0A"
 #track = "ASg+ATI0NDQrNCs1KzM2MzYyNzIgMiEyHDIcMRoxIDEfMR4wGTAZLxgvHS8cLhcuFy0cLRwsFywYKxwrHCoYKhgpHCkcKBgoGCccJxwmGSYZJR0lHSQZJBojHiMdIhsiHiIeIRshHyEfICAgGyAcHxsfKB8oHhseHB4bHhsdKB0oHBwcHBspGygaGhoaGSgZJxgaGBoXJhclFhoWGhUlFSUUGhQaEyUTJRIZEhkRJRElEBgQGA8XDyUPJQ4mDh4OHw0fDh8NIA4hDiINJg0lDCIMIQ0hDCENHA4NDg0NHA0cDA0MDQsaCxkKDgoOCRcJEQkPCA4IDg8NDxYPFBANEA0RFBEUEhESDxEhICcgJyEgISEiJyInIygjIiMjJCkkKSUjJSMmKSYpJyMnIygpKCkpIikiKiEqLiouKy8rLyowKjArKyssKywsMCwvLC8tIS0iLSEtIi4pLiguKC8hLyEwLTAtLysvMi8yMDAwMDEzMTAxMzE1MjUxMS4yLiwuKSwpKysqLSooLCsuKzEiMSIzLjMqNCk0NjQkNCQzIDMgMhoyGjEZMRkwGDAXLxctFy4XLBgqGCYZJhgmGCUZJRkkGiQcIhwbGhsaGhsYGxUaFBoSGRIYEBgRFxAWERcRFhIWExcTExMTEg4SDg4PDg8JFAkUChoKHAwdDB0OHg4eDx8PHxAfDx8QIBAhDiENIw0hDSEOIg0oDScNKA0oGCkYKRkqGSoaKxorGywbKxwqHCodKx0qHSoeKx4qHiohKiAqISshKyIqIisjKyQtJCwlLCgtKCwoLCkxKTEqMioyKzQrNCw1LDQsNSw1KzUsNCw0LzUvNTA2MDYyNzM3Nw=="
