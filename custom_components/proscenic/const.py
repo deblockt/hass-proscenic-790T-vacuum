8029e0866ed9d087a85ce085018bc8966e488765
Thomas Deblock <deblock.thomas.62@gmail.com>
2021-07-28 14:03:41 +0200

    feat: use config_flow to add create entity

---
+++ custom_components/proscenic/const.py
@@ -0,0 +1,11 @@
+DOMAIN="proscenic"
+
+CONF_DEVICE_ID = 'deviceId'
+CONF_TOKEN = 'token'
+CONF_USER_ID = 'userId'
+CONF_SLEEP = 'sleep_duration_on_exit'
+CONF_AUTH_CODE = 'authCode'
+CONF_MAP_PATH = 'map_path'
+
+DEFAULT_CONF_SLEEP = 60
+DEFAULT_CONF_MAP_PATH = '/tmp/proscenic_vacuum_map.svg'
